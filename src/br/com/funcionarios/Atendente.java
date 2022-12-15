fecdc52e00f71bbf7e7412dfa431bb475e18a3ac
Maria Laura <marialauradev@gmail.com>
2022-12-14 21:02:05 -0300

    Adição dos métodos adiar e cancelar

--- src/br/com/funcionarios/Atendente.java
+++ src/br/com/funcionarios/Atendente.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-
 public class Atendente {
 
     public Atendente() {}
@@ -22,7 +21,7 @@ public class Atendente {
         if(!cpfCadastrado){
             System.out.println("Digite seu nome: ");
             String nome = scanner.next();
-            System.out.print("Digite sua data de nascimento: (YYYY-MM-DD)");
+            System.out.print("Digite sua data de nascimento: (YYYY-MM-DD) ");
             LocalDate dataNascimento = LocalDate.parse(scanner.next());
             Paciente paciente = new Paciente.Builder()
                     .cpf(cpf)
@@ -108,4 +107,108 @@ public class Atendente {
         };
     }
 
+
+    public void adiarConsulta(Paciente paciente,ArrayList<Consulta> listaConsultas) {
+
+        List<Consulta> consultasMarcadas = listaConsultas.stream()
+                .filter(x -> {
+                    Paciente paciente1 = x.getPaciente();
+                    if (paciente1 == paciente) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                })
+                .collect(Collectors.toList());
+
+        if (!consultasMarcadas.isEmpty()) {
+            Scanner sc = new Scanner(System.in);
+            int i = 1;
+            System.out.println("------------Lista de consultas marcadas------------");
+            System.out.println("Paciente: " + paciente.getNome());
+            for (Consulta consulta : consultasMarcadas) {
+                System.out.printf(
+                        "------Consulta nº %d------\n" +
+                                "id: " + consulta.getId() + "\n" +
+                                "Data: " + consulta.getData() + "\n" +
+                                "Horario: " + consulta.getHorario() + "\n", i);
+                i++;
+            }
+
+            System.out.println("Informe o id da consulta que deseja adiar: ");
+            int identificador = sc.nextInt();
+
+            System.out.println("Escolha a nova data da sua consulta. Dias disponiveis: (YYYY-MM-DD)");
+            checarDiasDisponiveis(listaConsultas);
+            int novaData = sc.nextInt();
+            LocalDate data = LocalDate.now().plusDays(novaData - 1);
+
+            System.out.println("Escolha o novo horário da sua consulta. Horarios disponiveis: ");
+            checarHorariosDisponiveis(listaConsultas, data);
+            int novoHorario = sc.nextInt();
+            LocalTime horario = LocalTime.of(novoHorario + 7, 0, 0);
+
+            int novoIdentificador = Integer.parseInt("" + data.getYear() + data.getMonthValue() + data.getDayOfMonth() + horario.getHour());
+            for (Consulta consultas : listaConsultas) {
+                if (consultas.getId().compareTo(identificador) == 0) {
+                    consultas.setPaciente(null);
+                } else if (consultas.getId().compareTo(novoIdentificador) == 0) {
+                    consultas.setPaciente(paciente);
+                    System.out.println(consultas); // teste
+                    System.out.println(
+                            "------------Comprovante de reagendamento ------------\n" +
+                                    "Paciente: " + consultas.getPaciente().getNome() + "\n" +
+                                    "Novo id: " + consultas.getId() + "\n" +
+                                    "Nova data: " + consultas.getData() + "\n" +
+                                    "Novo horario: " + consultas.getHorario() + "\n");
+                }
+            }
+
+            System.out.println(listaConsultas);
+        } else {
+            System.out.println("----- Você ainda não tem consultas marcadas! -----");
+        }
+    }
+
+
+    public void cancelarConsulta(Paciente paciente,ArrayList<Consulta> listaConsultas){
+
+        List<Consulta> consultasMarcadas = listaConsultas.stream()
+                .filter(x -> {
+                    Paciente paciente1 = x.getPaciente();
+                    if (paciente1 == paciente){
+                        return true;
+                    }else {
+                        return false;
+                    }
+                })
+                .collect(Collectors.toList());
+
+        if (!consultasMarcadas.isEmpty() ){
+            Scanner sc = new Scanner(System.in);
+            int i = 1;
+            System.out.println("------------Lista de consultas marcadas------------");
+            System.out.println("Paciente: " + paciente.getNome());
+
+            for(Consulta consulta : consultasMarcadas){
+                System.out.printf(
+                        "------Consulta nº %d------\n" +
+                                "id: " + consulta.getId()+ "\n" +
+                                "Data: " + consulta.getData() + "\n" +
+                                "Horario: " + consulta.getHorario() + "\n", i);
+                i++;
+            }
+
+            System.out.println("Informe o id da consulta que deseja cancelar: ");
+            int identificador = sc.nextInt();
+
+            for(Consulta consultas : listaConsultas){
+                if (consultas.getId() == identificador){
+                    consultas.setPaciente(null);
+                }
+            }
+        } else {
+            System.out.println("----- Você ainda não tem consultas marcadas! -----");
+        }
+    }
 }
